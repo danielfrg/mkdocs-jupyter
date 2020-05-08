c0b282dfc123ec163bc7b65c4a833f77e97e9911
Daniel Rodriguez <daniel@danielfrg.com>
2020-05-07 21:40:25 -0500

    Code structure

--- mkdocs_jupyter/tests/test_base_usage.py
+++ mkdocs_jupyter/tests/test_base_usage.py
@@ -3,11 +3,10 @@ import subprocess
 
 import pytest
 
-CURDIR = os.path.dirname(os.path.realpath(__file__))
-
 
 @pytest.mark.parametrize("filename", ["mkdocs-without-nbs", "mkdocs-with-nbs"])
 def test_can_render_notebook(filename):
-    fpath = os.path.join(CURDIR, f"{filename}.yml")
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    fpath = os.path.join(this_dir, f"{filename}.yml")
     run = subprocess.run(["mkdocs", "build", "-q", "-f", fpath])
     assert run.returncode == 0
