eef5eab33d00967b3feb2f59a43f8e380d569fe5
khuynh <hkennyv@gmail.com>
2021-03-13 12:00:40 -0800

    Add test

--- mkdocs_jupyter/tests/test_base_usage.py
+++ mkdocs_jupyter/tests/test_base_usage.py
@@ -14,3 +14,15 @@ def test_can_render_notebook(filename):
 
     run = subprocess.run(["mkdocs", "build", "-q", "-f", f"{filename}"], cwd=mkdocs_dir)
     assert run.returncode == 0
+
+
+@pytest.mark.parametrize(
+    "filename",
+    ["mkdocs-without-pys.yml", "mkdocs-with-pys.yml", "mkdocs-with-pys-material.yml"],
+)
+def test_can_render_py(filename):
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    mkdocs_dir = os.path.join(this_dir, "mkdocs")
+
+    run = subprocess.run(["mkdocs", "build", "-q", "-f", f"{filename}"], cwd=mkdocs_dir)
+    assert run.returncode == 0
