34049889f02a44419fe3035317d41540261a4345
Quentin Fortier <qpfortier@gmail.com>
2021-11-11 21:52:42 +0100

    Fix accent problems in anchor link
    Accents (é, à) were not treated properly. I found that moving slugify fix the problem.

--- mkdocs_jupyter/convert.py
+++ mkdocs_jupyter/convert.py
@@ -24,8 +24,8 @@ def add_anchor_lower_id(html, anchor_link_text="¶"):
     except Exception:
         # failed to parse, just return it unmodified
         return html
-    link = _convert_header_id(html2text(h))
-    h.set("id", slugify(link))
+    link = _convert_header_id(slugify(html2text(h)))
+    h.set("id", link)
     a = Element("a", {"class": "anchor-link", "href": "#" + link})
     try:
         # Test if the anchor link text is HTML (e.g. an image)
